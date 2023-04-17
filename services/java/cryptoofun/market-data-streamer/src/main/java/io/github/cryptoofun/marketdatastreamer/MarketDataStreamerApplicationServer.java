feef4f2abec43ccf8a809891c2da6e53cf48535a
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-04-17 16:16:26 +0300

    [market-data] Complete the initial implementation

---
+++ services/java/cryptoofun/market-data-streamer/src/main/java/io/github/cryptoofun/marketdatastreamer/MarketDataStreamerApplicationServer.java
@@ -0,0 +1,12 @@
+package io.github.cryptoofun.marketdatastreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataStreamerApplicationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataStreamerApplicationServer.class, args);
+    }
+}
