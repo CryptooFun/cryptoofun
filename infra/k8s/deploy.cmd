f3449297d8f74e8480fce4a64e809357145e6cac
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-04-28 03:02:01 +0300

    Add k8s manifests and misc. scripts

---
+++ infra/k8s/deploy.cmd
@@ -0,0 +1,4 @@
+kubectl apply -f .\platform\ --namespace=cf
+kubectl apply -f .\networking\ --namespace=cf
+@REM kubectl apply -f .\security\ --namespace=cf
+@REM kubectl apply -f .\security\authn-policies\ --namespace=cf
