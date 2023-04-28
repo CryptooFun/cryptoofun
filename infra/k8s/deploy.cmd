dff2f1066e0fc946806303830555f7636b4b0c90
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-04-29 02:24:44 +0300

    infra: Move ScyllaDB to its own namespace

--- infra/k8s/deploy.cmd
+++ infra/k8s/deploy.cmd
@@ -1,3 +1,4 @@
+kubectl apply -f .\platform\scylladb --namespace=scylla
 kubectl apply -f .\platform\ --namespace=cf
 kubectl apply -f .\networking\ --namespace=cf
 @REM kubectl apply -f .\security\ --namespace=cf
