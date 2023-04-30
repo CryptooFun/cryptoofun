de4ef39ca8e2d82b4541afc65053e40b6841bb09
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-04-30 21:19:19 +0300

    Containerize NodeJS service(s)

---
+++ services/nodejs/cryptoofun/docker-build-push.cmd
@@ -0,0 +1,5 @@
+docker build . -f .\cash-wallet\Dockerfile -t gcr.io/cryptoofun/cash-wallet:1.0-SNAPSHOT
+docker push gcr.io/cryptoofun/cash-wallet:1.0-SNAPSHOT
+
+docker build . -f .\portfolio\Dockerfile -t gcr.io/cryptoofun/portfolio:1.0-SNAPSHOT
+docker push gcr.io/cryptoofun/portfolio:1.0-SNAPSHOT
