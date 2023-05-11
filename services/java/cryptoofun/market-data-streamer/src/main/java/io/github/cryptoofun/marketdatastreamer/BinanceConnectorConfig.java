f765805b6144ca9c1f668329b3ae15b243e23f93
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-05-11 22:43:02 +0300

    (marketdata): Authorize Binance spot client if possible

--- services/java/cryptoofun/market-data-streamer/src/main/java/io/github/cryptoofun/marketdatastreamer/BinanceConnectorConfig.java
+++ services/java/cryptoofun/market-data-streamer/src/main/java/io/github/cryptoofun/marketdatastreamer/BinanceConnectorConfig.java
@@ -1,14 +1,25 @@
 package io.github.cryptoofun.marketdatastreamer;
 
 import com.binance.connector.client.impl.SpotClientImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BinanceConnectorConfig {
 
+    @Value("${binance.api-key}")
+    private String apiKey;
+
+    @Value("${binance.secret-key}")
+    private String secretKey;
+
     @Bean
     SpotClientImpl binanceClient() {
-        return new SpotClientImpl();
+        if (apiKey.isBlank() || secretKey.isBlank()) {
+            System.out.println("Using Binance spot client in unauthorized mode!!");
+            return new SpotClientImpl();
+        }
+        return new SpotClientImpl(apiKey, secretKey);
     }
 }
