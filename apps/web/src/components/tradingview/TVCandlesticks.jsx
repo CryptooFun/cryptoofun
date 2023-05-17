0cd2e525fdb3d0c20eb98c71801ddd2612670051
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-05-17 22:19:51 +0300

    (webapp) Switch to TradingView advanced charts

--- apps/web/src/components/tradingview/TVCandlesticks.jsx
+++ apps/web/src/components/tradingview/TVCandlesticks.jsx
@@ -1,52 +1,58 @@
-import { createChart, CrosshairMode } from 'lightweight-charts';
 import React, { useEffect, useRef } from 'react';
 
-function TVCandlesticks({ initData, addData, height = 400 }) {
-  const chartContainerRef = useRef();
-  const candleSeries = useRef();
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget({ symbol }) {
+  const onLoadScriptRef = useRef();
 
   useEffect(() => {
-    const handleResize = () => {
-      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
-    };
+    onLoadScriptRef.current = createWidget;
 
-    const chart = createChart(chartContainerRef.current, {
-      layout: {
-        background: {
-          color: '#000000',
-        },
-        textColor: '#d1d4dc',
-      },
-      grid: {
-        horzLines: { color: '#111212' },
-        vertLines: { color: '#111212' },
-      },
-      height,
-      width: chartContainerRef.current.clientWidth,
-      crosshair: {
-        mode: CrosshairMode.Normal,
-      },
-    });
-    chart.timeScale().fitContent();
+    if (!tvScriptLoadingPromise) {
+      tvScriptLoadingPromise = new Promise(resolve => {
+        const script = document.createElement('script');
+        script.id = 'tradingview-widget-loading-script';
+        script.src = 'https://s3.tradingview.com/tv.js';
+        script.type = 'text/javascript';
+        script.onload = resolve;
 
-    candleSeries.current = chart.addCandlestickSeries();
-    candleSeries.current.setData(initData || []);
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      chart.remove();
-    };
-  }, [initData, height]);
-
-  useEffect(() => {
-    if (candleSeries.current) {
-      candleSeries.current.update(addData);
+        document.head.appendChild(script);
+      });
     }
-  }, [addData]);
 
-  return <div ref={chartContainerRef} />;
+    tvScriptLoadingPromise.then(
+      () => onLoadScriptRef.current && onLoadScriptRef.current()
+    );
+
+    return () => (onLoadScriptRef.current = null);
+
+    function createWidget() {
+      if (
+        document.getElementById('tradingview_51965') &&
+        'TradingView' in window
+      ) {
+        new window.TradingView.widget({
+          autosize: false,
+          width: 910,
+          height: 610,
+          symbol: `${symbol}USDT`,
+          interval: 'D',
+          timezone: 'Etc/UTC',
+          theme: 'dark',
+          style: '1',
+          locale: 'en',
+          toolbar_bg: '#f1f3f6',
+          enable_publishing: false,
+          allow_symbol_change: true,
+          container_id: 'tradingview_51965',
+        });
+      }
+    }
+  }, [symbol]);
+
+  return (
+    <div className="tradingview-widget-container">
+      <div id="tradingview_51965"></div>
+    </div>
+  );
 }
-
-export default TVCandlesticks;
--- apps/web/src/pages/trade/[tickerSymbol].js
+++ apps/web/src/pages/trade/[tickerSymbol].js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import TVCandlesticks from '@/components/tradingview/TVCandlesticks';
-import dayjs from 'dayjs';
 
 function TradingPage() {
   const router = useRouter();
@@ -17,132 +16,37 @@ function TradingPage() {
     ),
   });
 
+  const [symbol, setSymbol] = useState();
+
   const [buyAmount, setBuyAmount] = useState(1.0);
   const [sellAmount, setSellAmount] = useState(1.0);
 
-  const data = [
-    {
-      time: dayjs('2018-10-19').unix(),
-      open: 54.62,
-      high: 55.5,
-      low: 54.52,
-      close: 54.9,
-    },
-    {
-      time: dayjs('2018-10-22').unix(),
-      open: 55.08,
-      high: 55.27,
-      low: 54.61,
-      close: 54.98,
-    },
-    {
-      time: dayjs('2018-10-23').unix(),
-      open: 56.09,
-      high: 57.47,
-      low: 56.09,
-      close: 57.21,
-    },
-    {
-      time: dayjs('2018-10-24').unix(),
-      open: 57.0,
-      high: 58.44,
-      low: 56.41,
-      close: 57.42,
-    },
-    {
-      time: dayjs('2018-10-25').unix(),
-      open: 57.46,
-      high: 57.63,
-      low: 56.17,
-      close: 56.43,
-    },
-    {
-      time: dayjs('2018-10-26').unix(),
-      open: 56.26,
-      high: 56.62,
-      low: 55.19,
-      close: 55.51,
-    },
-    {
-      time: dayjs('2018-10-29').unix(),
-      open: 55.81,
-      high: 57.15,
-      low: 55.72,
-      close: 56.48,
-    },
-    {
-      time: dayjs('2018-10-30').unix(),
-      open: 56.92,
-      high: 58.8,
-      low: 56.92,
-      close: 58.18,
-    },
-    {
-      time: dayjs('2018-10-31').unix(),
-      open: 58.32,
-      high: 58.32,
-      low: 56.76,
-      close: 57.09,
-    },
-    {
-      time: dayjs('2018-11-01').unix(),
-      open: 56.98,
-      high: 57.28,
-      low: 55.55,
-      close: 56.05,
-    },
-    {
-      time: dayjs('2018-11-02').unix(),
-      open: 56.34,
-      high: 57.08,
-      low: 55.92,
-      close: 56.63,
-    },
-    {
-      time: dayjs('2018-11-05').unix(),
-      open: 56.51,
-      high: 57.45,
-      low: 56.51,
-      close: 57.21,
-    },
-    {
-      time: dayjs('2018-11-06').unix(),
-      open: 57.02,
-      high: 57.35,
-      low: 56.65,
-      close: 57.21,
-    },
-  ];
-
-  const [lastBusinessDay, setLastBusinessDay] = useState(dayjs('2018-11-07'));
-  const [addData, setAddData] = useState({
-    time: lastBusinessDay.unix(),
-    open: 57.02 - Math.random() * 2,
-    high: 57.35 - Math.random() * 2 + Math.random() * 2,
-    low: 56.65 - Math.random() * 2 + Math.random() * 2,
-    close: 55 + Math.random() * 2,
-  });
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      setAddData({
-        time: lastBusinessDay.unix(),
-        open: 57.02 - Math.random() * 2,
-        high: 57.35 - Math.random() * 2 + Math.random() * 2,
-        low: 56.65 - Math.random() * 2 + Math.random() * 2,
-        close: 55 + Math.random() * 2,
-      });
+    if (router.query.tickerSymbol) {
+      const pair0 = router.query.tickerSymbol.split(/[-_\s]/)[0];
+      setSymbol(pair0);
+    }
+  }, [router.query.tickerSymbol, setSymbol]);
 
-      const nextBusinessDay = lastBusinessDay.add(1, 'day');
-      setLastBusinessDay(nextBusinessDay);
-    }, 500);
-    return () => clearInterval(interval);
-  }, [lastBusinessDay]);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (
+  //       document.getElementById('header-toolbar-symbol-search') &&
+  //       document.getElementById('header-toolbar-compare')
+  //     ) {
+  //       document.getElementById('header-toolbar-symbol-search').disabled = true;
+  //       document.getElementById('header-toolbar-compare').remove();
+  //     }
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <>
       <br />
-      <TVCandlesticks initData={data} addData={addData}></TVCandlesticks>
+
+      {/* TODO: 4 hours window */}
+      {symbol ? <TVCandlesticks symbol={symbol}></TVCandlesticks> : <></>}
 
       {mutation.isSuccess ? <div>Success!</div> : <></>}
       <p>{router.query.tickerSymbol}</p>
