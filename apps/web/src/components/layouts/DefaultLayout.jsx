c9844e12c6fe92bb829e71d7ea53bd612ffe66bf
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-05-19 17:48:15 +0300

    (webapp): Move background image and components

--- apps/web/src/components/layouts/DefaultLayout.jsx
+++ apps/web/src/components/layouts/DefaultLayout.jsx
@@ -1,13 +1,22 @@
-import Content from '../Content';
 import Footer from '../Footer';
 import Header from '../Header';
+import Image from 'next/image';
 
 function DefaultLayout({ children }) {
   return (
     <>
       <Header />
+      <div className="bg-dark w-full justify-center items-center h-full">
+        <Image
+          className="max-h-screen mx-auto transparent"
+          src={'/bg.png'}
+          alt="BackGround"
+          fill
+        />
+      </div>
+
       {children}
-      <Content />
+
       <div className="bottom-0 mx-auto w-full fixed">
         <Footer />
       </div>
