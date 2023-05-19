194107ce533c4a945f2bd2565c94ae07b313f5cf
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-05-20 00:48:56 +0300

    (webapp): Set font to 'DM Sans'

--- apps/web/src/pages/_app.js
+++ apps/web/src/pages/_app.js
@@ -1,7 +1,13 @@
 import '@/globals.css';
 
+import { DM_Sans } from 'next/font/google';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin', 'latin-ext'],
+});
 
 const queryClient = new QueryClient();
 
@@ -9,7 +15,9 @@ export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <div className={dmSans.className}>
+          <Component {...pageProps} />
+        </div>
       </QueryClientProvider>
     </UserProvider>
   );
