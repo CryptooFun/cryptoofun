cde4753bacc897ba12b3f0245c8348c71aa340b7
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-05-23 02:00:35 +0300

    (webapp): Update protected pages matcher

--- apps/web/src/middleware.js
+++ apps/web/src/middleware.js
@@ -3,7 +3,10 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ['/trade/:path*', '/lobby/:path*',],
+  matcher: [
+    '/trade/:path*',
+    '/lobbies/:path*',
+    '/assets:path*',
+    '/profile:path*',
+  ],
 };
-
-// '/wallet'
