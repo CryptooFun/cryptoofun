88501725b4174b8e03aca86d43670d94b615c7dd
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-05-27 04:38:40 +0300

    (java): Update Cassandra and Kafka configurations + Bump to 1.0.0
    Removes ScyllaDB-specific configuration options and dependencies.

---
+++ services/java/cryptoofun/trade-butler/src/main/java/io/github/cryptoofun/tradebutler/config/CassandraConfiguration.java
@@ -0,0 +1,38 @@
+package io.github.cryptoofun.tradebutler.config;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Configuration
+@EnableCassandraRepositories(basePackages = {"io.github.cryptoofun.tradebutler"})
+public class CassandraConfiguration {
+
+    @Value("${spring.data.cassandra.keyspace}")
+    private String keyspace;
+
+    @Value("${spring.data.cassandra.username}")
+    private String username;
+
+    @Value("${spring.data.cassandra.password}")
+    private String password;
+
+    @Value("${spring.data.cassandra.cloudConnectBundleUrl}")
+    private String cloudConnectBundle;
+
+    @Bean
+    @Primary
+    protected CqlSession cqlSession() throws MalformedURLException {
+        return CqlSession.builder()
+                .withCloudSecureConnectBundle(new URL(cloudConnectBundle))
+                .withAuthCredentials(username, password)
+                .withKeyspace(keyspace)
+                .build();
+    }
+}
