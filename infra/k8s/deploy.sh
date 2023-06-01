e7bb1ef7c01448566340c69195c765ec01920a1d
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-06-02 00:03:34 +0300

    infra(k8s): Fix bash scripts

--- infra/k8s/deploy.sh
+++ infra/k8s/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f .\platform\ --namespace=cf
-kubectl apply -f .\networking\ --namespace=cf
-kubectl apply -f .\security\ --namespace=cf
-kubectl apply -f .\security\authn-policies\ --namespace=cf
+kubectl apply -f ./platform/ --namespace=cf
+kubectl apply -f ./networking/ --namespace=cf
+kubectl apply -f ./security/ --namespace=cf
+kubectl apply -f ./security/authn-policies/ --namespace=cf
